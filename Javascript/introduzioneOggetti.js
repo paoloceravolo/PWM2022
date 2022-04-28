5c12170831701fda010962d8f838d6bc44255382
Paolo Ceravolo <paoloceravolo@Abstrahere.local>
2022-04-28 13:16:31 +0200

    javascript

--- Javascript/introduzioneOggetti.js
+++ Javascript/introduzioneOggetti.js
@@ -6,4 +6,64 @@ let user = {
 };
 user.isAdmin = true;
 //delete user.age;
-console.log(user.isOld());
+let arr = []
+for(key in user){
+	arr.push(key);
+	//console.log(key);
+}
+console.log(arr)
+console.log(user);
+
+//let somma = function(x,y,z){
+//	return x+y;
+//}
+
+function Somma(x,y){
+	return x+y;
+}
+
+let somma = (x,y) => x+y;
+
+console.log(somma(3,7));
+
+var numeri = {
+	x: 12,
+	y: 3,
+	calcola: function(operazione) {
+		var fn;
+		switch (operazione) {
+			case '+':
+				fn = function() { return this.x + this.y};
+			break;
+			case '-':
+				fn = function() { return this.x - this.y};
+			break;
+			default:
+				fn = function() {};
+		}
+		return fn();
+	}
+} 
+console.log(numeri.calcola('+'));
+
+var numeri = {
+	x: 12,
+	y: 3,
+	calcola: function(operazione) {
+		var fn;
+		switch (operazione) {
+			case '+':
+				fn = () => this.x + this.y;
+			break;
+			case '-':
+				fn = () => this.x - this.y
+			break;
+			default:
+				fn = () => {};
+		}
+		return fn();
+	}
+} 
+console.log(numeri.calcola('-'));
+
+
