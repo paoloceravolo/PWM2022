649c15c300dafcf920d463cbc95e6a8219f75c5f
Paolo Ceravolo <paoloceravolo@Abstrahere.local>
2022-05-13 11:17:44 +0200

    Node intro

--- Node/primoserver.js
+++ Node/primoserver.js
@@ -1,4 +1,19 @@
-const id = process.id;
-const arr = process.argv;
+const http = require('http');
+const fs = require('fs');
 
-console.log("Buongiorno", arr[2],'!');
+const port = 8383;
+var s = http.createServer();
+s.listen(port);
+
+console.log('Server attivo su ', port);
+
+s.on('request', function(req,res){
+	let url = req.url;
+	console.log(url);
+	fs.readFile('./index.html', function(err,data){
+		if(err) throw err;
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(data);
+	});
+	console.log('Connessione attiva');
+});
